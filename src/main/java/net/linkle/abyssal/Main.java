3052938e4421aba68028977bef193115ece46aba
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2022-06-22 09:26:52 +1000

    setup

--- src/main/java/net/linkle/abyssal/Main.java
+++ src/main/java/net/linkle/abyssal/Main.java
@@ -1,13 +1,13 @@
-package net.fabricmc.example;
+package net.linkle.abyssal;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class Main implements ModInitializer {
+	public static final String ID = "abyssal_spider";
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
@@ -17,5 +17,9 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+	}
+	
+	public static Identifier makeId(String id) {
+	    return new Identifier(ID, id);
 	}
 }
